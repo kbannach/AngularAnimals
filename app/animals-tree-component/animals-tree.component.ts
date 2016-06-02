7f46173f774acbb323c59df837134750074f3fd3
kbannach <krzysztof92b@gmail.com>
2016-06-02 23:37:12 +0200

    dodanie edycji do komponentu drzewa

--- app/animals-tree-component/animals-tree.component.ts
+++ app/animals-tree-component/animals-tree.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { AnimalTreeNode } from '../animal-tree-node/animal-tree-node';
 import { AddChildComponent } from '../add-child-component/add-child.component';
+import { EditChildComponent } from '../edit-child-component/edit-child.component';
+
 @Component({
   selector: 'an-animals-tree',
   templateUrl: 'app/animals-tree-component/animals-tree.component.html',
-  directives: [AnimalsTreeComponent,AddChildComponent]
+  directives: [AnimalsTreeComponent, AddChildComponent, EditChildComponent]
 })
 export class AnimalsTreeComponent {
-    @Input() root: AnimalTreeNode;
+  @Input() root: AnimalTreeNode;
 }
